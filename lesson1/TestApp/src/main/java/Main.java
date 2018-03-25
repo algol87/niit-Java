2e255f8409cbfa660e0dc8c4f59b28fb5877df09
Ibrig <algolhate@gmail.com>
2018-03-25 09:25:37 +0300

    lesson1

---
+++ lesson1/TestApp/src/main/java/Main.java
@@ -0,0 +1,23 @@
+class Calc
+{
+    public static int add(int x,int y){
+        return x+y;
+    }
+    public static int sub(int x,int y){
+        return x-y;
+    }
+    public static int mul(int x,int y){
+        return x*y;
+    }
+    public static int div(int x,int y){
+        return x/y;
+    }
+}
+
+public class Main {
+    public static void main(String[] args){
+        int x=5,y=6,z;
+        z=Calc.add(x,y);
+        System.out.print("Сумма "+x+" и "+y+" равна "+z);
+    }
+}
